4cdc76f8d98123fc1da4ad2a339db6c4c4a1dd6c
Shivam <shekharshivam51@gmail.com>
2023-12-28 18:16:48 +0530

    fourth commit

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,13 +1,34 @@
-import axios from "axios"
+// import axios from "axios"
 
-export const axiosInstance = axios.create({});
+// export const axiosInstance = axios.create({});
+
+// export const apiConnector = (method, url, bodyData, headers, params) => {
+//     return axiosInstance({
+//         method:`${method}`,
+//         url:`${url}`,
+//         data: bodyData ? bodyData : null,
+//         headers: headers ? headers: null,
+//         params: params ? params : null,
+//     });
+// }
+
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: "https://study-notion-backend-9tnx.onrender.com/api/v1", // Replace with your actual backend URL
+  withCredentials: true, // Allow cookies to be sent with the request (if applicable)
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "https://study-notion-nine-sepia.vercel.app/api/v1", // Replace with your frontend origin
+  },
+});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
-    return axiosInstance({
-        method:`${method}`,
-        url:`${url}`,
-        data: bodyData ? bodyData : null,
-        headers: headers ? headers: null,
-        params: params ? params : null,
-    });
-}
+  return axiosInstance({
+    method: `${method}`,
+    url: `${url}`,
+    data: bodyData || null,
+    headers: headers || null,
+    params: params || null,
+  });
+};
