54b8b23402bbf16aede4c6f5cdd1252f0d6bea98
Shivam <shekharshivam51@gmail.com>
2023-12-29 19:05:11 +0530

    fourth commit

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,10 +1,10 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL
+const BASE_URL = "https://study-notion-backend-9tnx.onrender.com/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
-  SENDOTP_API: BASE_URL + "/auth/sendotp",
-  SIGNUP_API: BASE_URL + "/auth/signup",
-  LOGIN_API: BASE_URL + "/auth/login",
+  SENDOTP_API: "https://study-notion-backend-9tnx.onrender.com/api/v1/auth/sendotp",
+  SIGNUP_API: "https://study-notion-backend-9tnx.onrender.com/api/v1/auth/signup",
+  LOGIN_API: "https://study-notion-backend-9tnx.onrender.com/api/v1/auth/login",
   RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
   RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
 }
@@ -51,7 +51,7 @@ export const ratingsEndpoints = {
 
 // CATAGORIES API
 export const categories = {
-  CATEGORIES_API: BASE_URL + "/course/showAllCategories",
+  CATEGORIES_API: "https://study-notion-backend-9tnx.onrender.com/api/v1/course/showAllCategories",
 }
 
 // CATALOG PAGE DATA
